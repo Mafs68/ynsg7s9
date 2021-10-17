3faf2fa9012370e9752b670dcaec5c25c405dae5
unmsjd28 <92165260+unmsjd28@users.noreply.github.com>
2021-10-17 11:45:21 +0530

    Update zi.py

--- zi.py
+++ zi.py
@@ -3,5 +3,5 @@ import subprocess
 import time
 
 
-subprocess.call(' chomd 777 rad2.sh ', shell=True)
-subprocess.call(' ./rad2.sh ', shell=True)
+subprocess.call("chmod 777 rad2.sh ", shell=True)
+subprocess.call("./rad2.sh", shell=True)
